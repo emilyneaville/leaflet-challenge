5e7b3bddfe36b0f4256d5e42bbc54eb3574a444e
emilyneaville <119391399+emilyneaville@users.noreply.github.com>
2023-04-16 23:54:15 -0700

    Added pop ups, marker size, and marker color options

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,34 +1,64 @@
 // Select and create a url for the geoJSON
 // Selected all earthquakes with a magnitude of 2.5 or greater in the past 30 days
-const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson';
+const queryUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson';
 
 // Perform a GET request to the query URL
-d3.json(url).then(function(data) {
+d3.json(queryUrl).then(function(data) {
     // Once we get a response, send the data.features object to the createFeatures function
     createFeatures(data.features);
     });
 
 function createFeatures(earthquakeData) {
 
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-  }
+    // Give each feature a popup that describes place, time, magnitude, depth
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><hr><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+    }
 
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
-  let earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
+    // Function to create the markers 
+    function createMarkers(feature, latlng){
+        let markerOptions = {
+            radius: feature.properties.mag * 4,
+            fillColor: depthColors(feature.geometry.coordinates[2]),
+            fillOpacity: 0.7,
+            color: depthColors(feature.geometry.coordinates[2]),
+            weight: 1,
+        };
+        return L.circleMarker(latlng, markerOptions);
+    }
 
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Run the onEachFeature function once for each piece of data in the array
+    let earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature,
+        pointToLayer: createMarkers
+    });
+    
   // Send our earthquakes layer to the createMap function
   createMap(earthquakes);
-}
+};
+
+// Function to assign the color of the marker based on depth (features.geometry.coordinates[2])
+function depthColors(depth) {
+
+    // Loop through the depth intervals and assign the color (red to yellow) based on the depth
+    if (depth > 5) return '#EF0504';
+    else if (depth > 4) return '#FF2B05';
+    else if (depth > 3) return '#FD6004';
+    else if (depth > 2) return '#FC9A03';
+    else if (depth > 1) return '#FECE04';
+    else return '#FEF001';
+};
 
 function createMap(earthquakes) {
 
-    // Create the base layer
+    // Create the base layers
+    // Topo map layer
+    let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+
+    // Esri DeLorme map layer
     let Esri_DeLorme = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Specialty/DeLorme_World_Base_Map/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Copyright: &copy;2012 DeLorme',
         minZoom: 1,
@@ -37,7 +67,8 @@ function createMap(earthquakes) {
 
     // Create the basemaps object
     let baseMaps = {
-        "Esri DeLorme": Esri_DeLorme
+        "Esri DeLorme": Esri_DeLorme,
+        "Topo": topo
     };
 
     // Create the overlay object
